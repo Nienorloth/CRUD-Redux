d5d9a65a320e24cfb1041506118e592bdc5e019d
Mariana Rascon <nienorloth@gmail.com>
2020-08-04 18:05:03 -0500

    Starting with Redux

---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+//Actions de Redux
+import { crearNuevoProductoAction } from '../actions/productoActions'
+
+
+const NuevoProducto = () => {
+
+    //utilizar use dispatch y te crea una función
+    const dispatch = useDispatch();
+
+    //mandar llamar el action de productoAction
+    const agregarProducto = () => dispatch( crearNuevoProductoAction() )
+
+    //Cuando el usuario haga submit
+    const submitNuevoProducto = e => {
+        e.preventDefault();
+
+        //validar formulario
+
+
+        //sino hay errores
+
+
+        //crear el nuevo producto
+        agregarProducto();
+
+    }
+
+
+    return ( 
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Agregar Nuevo Producto
+                        </h2>
+
+                        <form
+                            onSubmit={submitNuevoProducto}
+                        >
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Producto"
+                                    name="nombre"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio Producto</label>
+                                <input 
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio Producto"
+                                    name="precio"
+                                />
+                            </div>
+
+                            <button 
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                                >Agregar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default NuevoProducto;
