aa68c401a6bca9f1900414eca7a03bd901294d56
Mariana Rascon <nienorloth@gmail.com>
2020-08-05 17:37:43 -0500

    productos reducer, actions and types for adding product created

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 //Actions de Redux
 import { crearNuevoProductoAction } from '../actions/productoActions'
 
 
-const NuevoProducto = () => {
+const NuevoProducto = ({history}) => {
+
+    //State del componente
+    const [nombre, guardarNombre] = useState('');
+    const [precio, guardarPrecio] = useState(0);
 
     //utilizar use dispatch y te crea una función
     const dispatch = useDispatch();
 
+    //Acceder al state del store
+    const cargando = useSelector(state => state.productos.loading);
+    const error = useSelector(state => state.productos.error);
+
+    console.log(cargando);
+
     //mandar llamar el action de productoAction
-    const agregarProducto = () => dispatch( crearNuevoProductoAction() )
+    const agregarProducto = producto => dispatch( crearNuevoProductoAction(producto) )
 
     //Cuando el usuario haga submit
     const submitNuevoProducto = e => {
         e.preventDefault();
 
         //validar formulario
+        if(nombre.trim() === '' || precio <= 0){
+            return;
+        }
 
 
         //sino hay errores
 
 
         //crear el nuevo producto
-        agregarProducto();
+        agregarProducto({
+            nombre,
+            precio
+        });
+
+        //redireccionar
+        history.push('/');
 
     }
 
@@ -48,6 +67,8 @@ const NuevoProducto = () => {
                                     className="form-control"
                                     placeholder="Nombre Producto"
                                     name="nombre"
+                                    value={nombre}
+                                    onChange={e => guardarNombre(e.target.value)}
                                 />
                             </div>
                             <div className="form-group">
@@ -57,6 +78,8 @@ const NuevoProducto = () => {
                                     className="form-control"
                                     placeholder="Precio Producto"
                                     name="precio"
+                                    value={precio}
+                                    onChange={e => guardarPrecio(Number(e.target.value))}
                                 />
                             </div>
 
@@ -65,6 +88,8 @@ const NuevoProducto = () => {
                                 className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
                                 >Agregar</button>
                         </form>
+                        { cargando ? <p>Cargando...</p> : null }
+                        { error ? <p className="alert alert-danger p2 mt-4 text-center">Hubo un error</p> : null }
                     </div>
                 </div>
             </div>
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -14,6 +14,24 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case AGREGAR_PRODUCTO:
+        case COMENZAR_DESCARGA_PRODUCTOS:
+            return {
+                ...state,
+                loading: action.payload
+            }
+        case AGREGAR_PRODUCTO_EXITO:
+            return {
+                ...state,
+                loading: false,
+                productos: [...state.productos, action.payload]
+            }
+        case AGREGAR_PRODUCTO_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
         default:
             return state;
     }
